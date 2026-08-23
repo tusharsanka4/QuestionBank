e409bfd5d7c0414b16730211f2beaf48fdec0d13
Tushar <tusharsanka4@gmail.com>
2026-08-23 18:57:28 +0530

    Changing the embedding system so that I dont use torch package which is 2gig and then regenerating requirements so that its easier to deploy backend

--- backend/ingest.py
+++ backend/ingest.py
@@ -3,13 +3,23 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from chromadb.utils.embedding_functions import DefaultEmbeddingFunction
+from langchain_community.vectorstores import Chroma
+from langchain.embeddings.base import Embeddings
 
 load_dotenv()
 
 CHROMA_DIR = "./chroma_db"
 
-embedder = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+class ChromaDefaultEmbedder(Embeddings):
+    def __init__(self):
+        self.fn = DefaultEmbeddingFunction()
+    def embed_documents(self, texts):
+        return self.fn(texts)
+    def embed_query(self, text):
+        return self.fn([text])[0]
+
+embedder = ChromaDefaultEmbedder()
 
 def ingest_pdf(file_path: str):
     loader = PyPDFLoader(file_path)
